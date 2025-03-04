5c3c963a9a60d3756b6e2dadcb6fb49a609fc6e2
Ijay Balan <iijaybalan@gmail.com>
2025-03-04 12:05:11 +0530

    updated HistoryPage in Component with new changes

--- src/Component/HistoryPage/HistoryPage.js
+++ src/Component/HistoryPage/HistoryPage.js
@@ -19,9 +19,9 @@ let HistoryPage = () => {
     return (
         <div>
             <div style={{backgroundColor:"skyblue",width:"100%"}}>
-                <div className="d-flex justify-content-between">
-                <img className="col-6 col-md-5 col-lg-4 col-xl-4 m-2 " src="https://computergyaan.in/wp-content/uploads/2018/10/canara-bank-logo.jpg" alt="Logo" />
-                <button className="col-4 col-md-2 col-lg-2 col-xl-1 m-4 m-md-5 m-xl-5 m-lg-5 btn btn-custom" style={{textAlign:"center",height:"10%",backgroundColor:'#FFBF00',borderColor:'#FFBF00'}} onClick={homepage} >HomePage</button>
+                <div className="d-flex justify-content-between align-items-center">
+                <img className="col-5 col-md-5 col-lg-4 col-xl-4 m-2 " src="https://computergyaan.in/wp-content/uploads/2018/10/canara-bank-logo.jpg" alt="Logo" />
+                <button className="col-3 col-md-1 col-lg-1 col-xl-1 p-1 m-2 m-md-5 m-xl-5 m-lg-5 btn btn-custom" style={{textAlign:"center",height:"10%",backgroundColor:'#FFBF00',borderColor:'#FFBF00',fontSize:"10px"}} onClick={homepage} >Home Page</button>
                 </div>
             </div>
             <div className="row" style={{width:"100%"}}>
@@ -40,7 +40,7 @@ let HistoryPage = () => {
                     </div>
                 );
             })}
-            <div className=" row d-flex justify-content-center " style={{marginTop:"60vh"}}>
+            <div className=" row d-flex justify-content-center " style={{marginTop:"60vh",width:"100%"}}>
                 <div className="col-8 col-md-6 col-lg-5 col-xl-4 col-xxl-3" style={{border:"1px solid",borderColor:"lightgray"}}>
                     <p className="d-flex justify-content-center align-items-center m-1" style={{color:"gray"}}> Note: Click HomePage to Go Back</p>
                 </div>
